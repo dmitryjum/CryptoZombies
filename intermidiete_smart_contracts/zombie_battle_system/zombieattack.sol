c7020ed9af99716cba94ae24a0517af5416c5d1d
Dmitry Jum <xfiler@ymail.com>
2024-04-15 21:34:03 -0700

    zombie battle system chapter 4: random numbers

--- intermidiete_smart_contracts/zombie_battle_system/zombieattack.sol
+++ intermidiete_smart_contracts/zombie_battle_system/zombieattack.sol
@@ -1,1 +1,12 @@
-zombieattack.sol
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+  uint randNonce = 0;
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+  }
+}
